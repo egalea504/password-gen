c809ceae3d6fcad0811cb57bf451dd26e964857e
eliza galea <111540402+egalea504@users.noreply.github.com>
2024-04-10 19:38:23 -0400

    setup routes app

--- client/src/App.js
+++ client/src/App.js
@@ -1,116 +1,25 @@
-// Style Import
-import "./App.css"
+// // Style Import
+// import "./App.css"
 
-// App.js
-import React, { useState, useEffect } from "react";
-import Axios from 'axios';
+// // App.js
+// import React, { useState, useEffect } from "react";
+// import Axios from 'axios';
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import SignIn from './pages/signin';
+import AddPasswordInfo from './pages/addPasswordInfo';
 
 const App = () => {
-
-	const [password, setPassword] = useState("");
-	const [title, setTitle] = useState("");
-	const [passwordList, setPasswordList] = useState([]);
-
-	useEffect(() => {
-		Axios.get('http://localhost:3001/showpasswords')
-		.then( (response) => {
-			console.log(response.data.rows);
-			setPasswordList(response.data.rows);
-		})
-		.catch((error) => {
-			if (error.reponse) {
-			console.log(error.response.data)
-			}
-		})
-		}, []);
-
-		const addPassword = () => {
-			if (password && title) {
-				Axios.post('http://localhost:3001/addpassword', {
-					password: password,
-					title: title
-				})
-					.then((response) => {
-						console.log('Password added:', { password, title });
-						setPassword('');
-						setTitle('');
-						console.log("It worked!!");
-					})
-					.catch((error) => {
-						if (error.response) {
-							console.log(error.response.data);
-						}
-					});
-			} else {
-				alert('Please fill out both password and title.');
-			}
-		};
-
-	const decryptPassword = (encryption) => {
-		Axios.post('http://localhost:3001/decryptpassword', {
-			password: encryption.password,
-			iv: encryption.iv
-		})
-		.then((response) => {
-			setPasswordList(passwordList.map((val) => {
-				return val.id === encryption.id
-				? {
-					id: val.id,
-					password: val.password,
-					title: response.data,
-					iv: val.id
-				} : val;
-		})
-		)
-		});
-	};
-
-	return (
-		<div className="App"> 
-			<div className="AddingPass">
-				<input 
-				type="text" 
-				placeholder="Ex: password123"
-				required
-				onChange={(event)=> {
-					setPassword(event.target.value);
-					console.log(password);
-				}}>
-				</input>
-
-				<input 
-				type="text" 
-				placeholder="Ex: Facebook"
-				required
-				onChange={(event)=> {
-					setTitle(event.target.value);
-					console.log(title);
-				}}
-				>
-				</input>
-				<button onClick={addPassword}>Add Password</button>
-			</div>
-
-			<div className="Passwords">
-				{passwordList.map((val, key) => {
-					return (<div className="Password"
-					onClick={
-						() => {
-							decryptPassword({
-								password: val.password,
-								iv: val.iv,
-								id: val.id
-							})
-						}}
-						key={key}
-						>
-					<h3>{val.title}</h3>
-					</div>
-				)
-				})}
-			</div>
-		</div>
-	)
+ return (
+    <>
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/addpassword" element={<AddPasswordInfo />} />
+       </Routes>
+    </>
+ );
 };
 
 export default App;
---
+++ client/src/pages/addPasswordInfo.js
@@ -0,0 +1,116 @@
+// // Style Import
+// import "./App.css"
+
+// App.js
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
+
+const AddPasswordInfo = () => {
+
+	const [password, setPassword] = useState("");
+	const [title, setTitle] = useState("");
+	const [passwordList, setPasswordList] = useState([]);
+
+	useEffect(() => {
+		Axios.get('http://localhost:3001/showpasswords')
+		.then( (response) => {
+			console.log(response.data.rows);
+			setPasswordList(response.data.rows);
+		})
+		.catch((error) => {
+			if (error.reponse) {
+			console.log(error.response.data)
+			}
+		})
+		}, []);
+
+		const addPassword = () => {
+			if (password && title) {
+				Axios.post('http://localhost:3001/addpassword', {
+					password: password,
+					title: title
+				})
+					.then((response) => {
+						console.log('Password added:', { password, title });
+						setPassword('');
+						setTitle('');
+						console.log("It worked!!");
+					})
+					.catch((error) => {
+						if (error.response) {
+							console.log(error.response.data);
+						}
+					});
+			} else {
+				alert('Please fill out both password and title.');
+			}
+		};
+
+	const decryptPassword = (encryption) => {
+		Axios.post('http://localhost:3001/decryptpassword', {
+			password: encryption.password,
+			iv: encryption.iv
+		})
+		.then((response) => {
+			setPasswordList(passwordList.map((val) => {
+				return val.id === encryption.id
+				? {
+					id: val.id,
+					password: val.password,
+					title: response.data,
+					iv: val.id
+				} : val;
+		})
+		)
+		});
+	};
+
+	return (
+		<div className="App"> 
+			<div className="AddingPass">
+				<input 
+				type="text" 
+				placeholder="Ex: password123"
+				required
+				onChange={(event)=> {
+					setPassword(event.target.value);
+					console.log(password);
+				}}>
+				</input>
+
+				<input 
+				type="text" 
+				placeholder="Ex: Facebook"
+				required
+				onChange={(event)=> {
+					setTitle(event.target.value);
+					console.log(title);
+				}}
+				>
+				</input>
+				<button onClick={addPassword}>Add Password</button>
+			</div>
+
+			<div className="Passwords">
+				{passwordList.map((val, key) => {
+					return (<div className="Password"
+					onClick={
+						() => {
+							decryptPassword({
+								password: val.password,
+								iv: val.iv,
+								id: val.id
+							})
+						}}
+						key={key}
+						>
+					<h3>{val.title}</h3>
+					</div>
+				)
+				})}
+			</div>
+		</div>
+	)
+};
+
+export default AddPasswordInfo;
